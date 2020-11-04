965abc61befb52482c4d821e4816e03574f5345c
Victor Flores <flores.vicf@gmail.com>
2020-11-04 11:11:25 -0500

    Initial Commit

---
+++ jornual/jornualApp.swift
@@ -0,0 +1,17 @@
+//
+//  jornualApp.swift
+//  jornual
+//
+//  Created by Victor Flores on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct jornualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
